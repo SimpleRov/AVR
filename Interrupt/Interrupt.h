2656fc43615c33b79cf8ab43759e4f932ce9186c
SimpleRov <alexanderhubkin@gmail.com>
2016-05-03 13:31:43 +0300

    Added Interrupt.
    1. External Interrupts.
    2. Pin change interrupt.

---
+++ Interrupt/Interrupt.h
@@ -0,0 +1,14 @@
+#ifndef INTERRUPT_H_
+#define INTERRUPT_H_
+
+/// <summary>
+/// Настройка внешнего прерывания 6.
+/// </summary>
+void SetupExtInterrupt6();
+
+/// <summary>
+/// Настройка прерывания по изменению состояния группы выводов 0.
+/// </summary>
+void SetupPortInterrupt0();
+
+#endif /* INTERRUPT_H_ */
